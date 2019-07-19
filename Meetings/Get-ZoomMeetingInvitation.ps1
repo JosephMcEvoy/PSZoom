6f64046db033da018696dce8feecd9ad1ddf68c9
JosephMcEvoy <joe.maci@gmail.com>
2019-07-19 15:22:05 -0400

    Added Get-ZoomMeetingInvitation.ps1

---
+++ Meetings/Get-ZoomMeetingInvitation.ps1
@@ -0,0 +1,60 @@
+<#
+
+.SYNOPSIS
+Retrieve the meeting invitation
+.DESCRIPTION
+Retrieve the meeting invitation
+.PARAMETER MeetingId
+The meeting ID.
+.PARAMETER ApiKey
+The Api Key.
+.PARAMETER ApiSecret
+The Api Secret.
+.EXAMPLE
+Get-ZoomMeetingInvitation 123456789
+
+
+#>
+
+$Parent = Split-Path $PSScriptRoot -Parent
+import-module "$Parent\ZoomModule.psm1"
+
+function Get-ZoomMeetingInvitation {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Mandatory = $True, 
+            ValueFromPipeline = $True, 
+            Position = 0
+        )]
+        [string]$MeetingId,
+
+        [string]$ApiKey,
+
+        [string]$ApiSecret
+    )
+
+    begin {
+        #Get Zoom Api Credentials
+        if (-not $ApiKey -or -not $ApiSecret) {
+            $ApiCredentials = Get-ZoomApiCredentials
+            $ApiKey = $ApiCredentials.ApiKey
+            $ApiSecret = $ApiCredentials.ApiSecret
+        }
+
+        #Generate JWT (JSON Web Token)
+        $Headers = New-ZoomHeaders -ApiKey $ApiKey -ApiSecret $ApiSecret
+    }
+
+    process {
+        $Uri = "https://api.zoom.us/v2/meetings/$MeetingId/invitation"
+        
+        try {
+            $Response = Invoke-RestMethod -Uri $Uri -Headers $headers -Body $RequestBody -Method GET
+        } catch {
+            Write-Error -Message "$($_.exception.message)" -ErrorId $_.exception.code -Category InvalidOperation
+        }
+        
+        Write-Output $Response
+    }
+}

8fe38bf8f94030501ec36656c1f9091903ab5a35
Joseph McEvoy <joe.maci@gmail.com>
2019-08-09 18:26:44 -0400

    Minor fixes.

--- Tests/test.ps1
+++ Tests/test.ps1
@@ -1,5 +1,5 @@
 Import-Module D:\Development\Zoom\PowerShell-Zoom-Wrapper\PowerShell-Zoom-Wrapper\PowerShell-Zoom-Wrapper.psm1
-$Global:ZoomApiKey = 'xemREbnhSGyiH9dZ32obng'
-$Global:ZoomApiSecret = '8InebJpSh00cbcf7tuRfKVRFFwUgFwbnjnwn'
+$Global:ZoomApiKey = ''
+$Global:ZoomApiSecret = ''
 
 Get-ZoomMeeting -MeetingId 112169160

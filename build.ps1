25f2dc210430c9c48904d287f43fa711835960c6
Joseph McEvoy <joe.maci@gmail.com>
2019-08-16 19:23:19 -0400

    Test of continuous deployment.

--- build.ps1
+++ build.ps1
@@ -1,5 +1,10 @@
-#Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-#Install-Module -name ('Psake', 'PSDeploy', 'Pester', 'BuildHelpers') -Force
-#Import-Module -name ('Psake', 'PSDeploy', 'Pester', 'BuildHelpers') -Force
-Set-BuildEnvironment -BuildOutput '.\'
-Get-BuildEnvironment
+param($Task = 'Default')
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+
+Install-Module 'Psake', 'PSDeploy', 'Pester', 'BuildHelpers'
+Import-Module 'Psake', 'PSDeploy', 'Pester', 'BuildHelpers' 
+
+Set-BuildEnvironment
+
+Invoke-psake .\psake.ps1 -taskList $Task -nologo
+exit ( [int]( -not $psake.build_success ) )

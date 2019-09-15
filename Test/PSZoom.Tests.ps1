4245b69d7227093218520b6db78a4e2549ba08e1
Joseph McEvoy <joe.maci@gmail.com>
2019-09-15 17:25:26 -0400

    !deploy

--- Test/PSZoom.Tests.ps1
+++ Test/PSZoom.Tests.ps1
@@ -4,15 +4,15 @@ $TestArchive = 'TestArchive'
 $TestProxy = 'TestProxy'
 
 
-#$PSVersion = $PSVersionTable.PSVersion.Major
-#$ModuleName = $ENV:BHProjectName
-#$ModulePath = Join-Path $ENV:BHProjectPath $ModuleName
+$PSVersion = $PSVersionTable.PSVersion.Major
+$ModuleName = $ENV:BHProjectName
+$ModulePath = Join-Path $ENV:BHProjectPath $ModuleName
 
 
 #Using these variables for local testing
-$PSVersion = $PSVersionTable.PSVersion.Major
-$ModuleName = 'PSZoom'
-$ModulePath = "d:\dev\$ModuleName\$ModuleName"
+#$PSVersion = $PSVersionTable.PSVersion.Major
+#$ModuleName = 'PSZoom'
+#$ModulePath = "d:\dev\$ModuleName\$ModuleName"
 
 
 # Verbose output for non-master builds on appveyor

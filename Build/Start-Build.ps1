31e491ff7c05d26cac64380c36fe8b417f9fb00d
Joseph McEvoy <joe.maci@gmail.com>
2019-09-18 10:16:39 -0400

    Test

--- Build/Start-Build.ps1
+++ Build/Start-Build.ps1
@@ -1,13 +1,15 @@
-﻿Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+﻿$env:PSModulePath = $env:PSModulePath + $( Join-Path ([Environment]::GetFolderPath('MyDocuments')) 'WindowsPowerShell\Modules')
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 if(-not (Get-Module -ListAvailable PSDepend)) {
-    install-module psdepend
+    & (Resolve-Path "$PSScriptRoot\helpers\Install-PSDepend.ps1")
 }
 
 Import-Module PSDepend
-$null = Invoke-PSDepend -Path "$PSScriptRoot\build.requirements.psd1" -Install -Import -Force
+
+Invoke-PSDepend -Path "$PSScriptRoot\build.requirements.psd1" -Install -Import -Force | Out-Null
 
 Set-BuildEnvironment -Force -Path $PSScriptRoot\..
 
-Invoke-psake $PSScriptRoot\psake.ps1 -nologo
-exit ( [int]( -not $psake.build_success ) )
+Invoke-psake $PSScriptRoot\psake.ps1
+exit ([int](-not $psake.build_success))

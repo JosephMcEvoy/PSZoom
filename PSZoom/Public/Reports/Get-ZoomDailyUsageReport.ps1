200ef3012eea004f6c9355d2d8bf9b6144f055c1
Joseph McEvoy <joe.maci@gmail.com>
2020-04-12 02:25:46 -0400

    Fixed Get-ZoomDailyUsageReport.

--- PSZoom/Public/Reports/Get-ZoomDailyUsageReport.ps1
+++ PSZoom/Public/Reports/Get-ZoomDailyUsageReport.ps1
@@ -25,46 +25,20 @@ A hastable with the Zoom API response.
 
 #>
 
-function Get-ZoomActiveInactiveHostReports {
+function Get-ZoomDailyUsageReport {
     [CmdletBinding(DefaultParameterSetName = 'Default')]
     param (
         [Parameter(
-            Mandatory = $True, 
-            ValueFromPipelineByPropertyName = $True,
-            ParameterSetName = 'Default'
-        )]
-        [ValidatePattern('([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))')]
-        [string]$From,
-
-        [Parameter(
-            Mandatory = $True, 
-            ParameterSetName = 'Default',
+            Mandatory = $True,
             ValueFromPipelineByPropertyName = $True
         )]
-        [ValidatePattern('([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))')]
-        [string]$To,
+        [int]$Year,
 
-        [Parameter(
-            ParameterSetName = 'Default',
+        [Parameter( 
+            Mandatory = $True,
             ValueFromPipelineByPropertyName = $True
         )]
-        [ValidateRange(1, 300)]
-        [Alias('size', 'page_size')]
-        [int]$PageSize = 30,
-
-        [Parameter(
-            ParameterSetName = 'Default',
-            ValueFromPipelineByPropertyName = $True
-        )]
-        [Alias('page', 'page_number')]
-        [int]$PageNumber = 1,
-
-        [Parameter(ParameterSetName = 'Default')]
-        [ValidateSet('active', 'inactive')]
-        [string]$Type = 'inactive', #Zoom defaults to inactive if this isn't sent in the query.
-
-        [Parameter(ParameterSetName = 'All')]
-        [switch]$All,
+        [int]$Month,
 
         [string]$ApiKey,
 
@@ -77,46 +51,30 @@ function Get-ZoomActiveInactiveHostReports {
     }
 
     process {
-        if ($All) {
-            [int]$requests = 0
-            $monthRanges = (Get-LastSixMonthsDateRanges)
-            $allReports = New-Object System.Collections.Generic.List[System.Object]
+        $Request = [System.UriBuilder]"https://api.zoom.us/v2/report/daily"
 
-            foreach ($key in $monthRanges.keys) {
-                $TotalPages = (Get-ZoomActiveInactiveHostReports -from "$($monthRanges.$key.begin)" -to "$($monthRanges.$key.end)" -pagesize 300 -pagenumber 1 -type active).page_count
-                
-                for ($i = 1; $i -le $TotalPages; $i++) {
-                    if (($requests % 10) -eq 0) { #Zoom limits the number of requests to 10 per second
-                        Start-Sleep -seconds 2
-                    }
-        
-                    $currentPage = (Get-ZoomActiveInactiveHostReports -from "$($monthRanges.$key.begin)" -to "$($monthRanges.$key.end)" -pagesize 300 -pagenumber $i -type active).users
-                    
-                    foreach ($entry in $currentPage) {
-                        $allReports.Add($entry)
-                    }
-        
-                    $requests++
-                }
-            }
-            write-output $allReports
-        } else {
-            $Request = [System.UriBuilder]"https://api.zoom.us/v2/report/users"
+        if ($Year -or $Query) {
             $query = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)  
-            $query.Add('from', $From)
-            $query.Add('to', $To)
-            $query.Add('page_size', $PageSize)
-            $query.Add('page_number', $PageNumber)
-            $query.Add('type', $Type)
-            $Request.Query = $query.ToString()
-            
-            try {
-                $response = Invoke-RestMethod -Uri $request.Uri -Headers $headers -Method GET
-            } catch {
-                Write-Error -Message "$($_.Exception.Message)" -ErrorId $_.Exception.Code -Category InvalidOperation
-            }
-            
-            Write-Output $response
         }
+        
+        if ($Year) {
+            $query.Add('year', $Year)
+        }
+
+        if ($Month) {
+            $query.Add('month', $MOnth)
+        }
+
+        if ($query) {
+            $Request.Query = $query.ToString()
+        }
+        
+        try {
+            $response = Invoke-RestMethod -Uri $request.Uri -Headers $headers -Method GET
+        } catch {
+            Write-Error -Message "$($_.Exception.Message)" -ErrorId $_.Exception.Code -Category InvalidOperation
+        }
+        
+        Write-Output $response
     }
 }

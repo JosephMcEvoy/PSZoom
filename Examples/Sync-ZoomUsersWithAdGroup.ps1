d777881ce26f3398cbb26d6b9542fa34b99e2e74
JosephMcEvoy <joe.maci@gmail.com>
2020-04-23 14:08:52 -0400

    Update Sync-ZoomUsersWithAdGroup.ps1

--- Examples/Sync-ZoomUsersWithAdGroup.ps1
+++ Examples/Sync-ZoomUsersWithAdGroup.ps1
@@ -1,8 +1,8 @@
 <#
 
 .DESCRIPTION
-This script takes an ADGroup, then compares the email addresses in the group against Zoom. Then it adds/removes users in order to "sync"
-the ADGroup with Zoom.
+This script takes an ADGroup, then compares the email addresses in the group against Zoom. Then it adds/removes users in order 
+to "sync" the ADGroup with Zoom.
 Any users in the AD Group who are not in Zoom are CREATED in Zoom if the -Add switch is specified.
 Any users in Zoom who are not in the AD Group are REMOVED from Zoom if the -Remove switch as specified.
 .PARAMETER AdGroup

b7d33c5cdad4a0bb5f1b93f26836e329e4db1bc3
Joseph McEvoy <joe.maci@gmail.com>
2021-04-28 00:56:33 -0400

    !deploy 12:56

--- Build/helpers/Install-PSDepend.ps1
+++ Build/helpers/Install-PSDepend.ps1
@@ -1,58 +1,60 @@
-    <#
-    .SYNOPSIS
-        Bootstrap PSDepend
+<#
 
-    .DESCRIPTION
-        Bootstrap PSDepend
+.SYNOPSIS
+    Bootstrap PSDepend
 
-        Why? No reliance on PowerShellGallery
+.DESCRIPTION
+    Bootstrap PSDepend
 
-          * Downloads nuget to your ~\ home directory
-          * Creates $Path (and full path to it)
-          * Downloads module to $Path\PSDepend
-          * Moves nuget.exe to $Path\PSDepend (skips nuget bootstrap on initial PSDepend import)
+    Why? No reliance on PowerShellGallery
 
-    .PARAMETER Path
-        Module path to install PSDepend
+        * Downloads nuget to your ~\ home directory
+        * Creates $Path (and full path to it)
+        * Downloads module to $Path\PSDepend
+        * Moves nuget.exe to $Path\PSDepend (skips nuget bootstrap on initial PSDepend import)
 
-        Defaults to Profile\Documents\WindowsPowerShell\Modules
+.PARAMETER Path
+    Module path to install PSDepend
 
-    .EXAMPLE
-        .\Install-PSDepend.ps1 -Path C:\Modules
+    Defaults to Profile\Documents\WindowsPowerShell\Modules
 
-        # Installs to C:\Modules\PSDepend
-    #>
-    [cmdletbinding()]
-    param(
-        [string]$Path = $( Join-Path ([Environment]::GetFolderPath('MyDocuments')) 'PowerShell\Modules')
-    )
+.EXAMPLE
+    .\Install-PSDepend.ps1 -Path C:\Modules
+
+    # Installs to C:\Modules\PSDepend
+
+#>
+
+[cmdletbinding()]
+param(
+    [string]$Path = $( Join-Path ([Environment]::GetFolderPath('MyDocuments')) 'PowerShell\Modules')
+)
     
-    $ExistingProgressPreference = "$ProgressPreference"
-    $ProgressPreference = 'SilentlyContinue'
+$ExistingProgressPreference = "$ProgressPreference"
+$ProgressPreference = 'SilentlyContinue'
 
-    try {
-        # Bootstrap nuget if we don't have it
-        if(-not ($NugetPath = (Get-Command 'nuget.exe' -ErrorAction SilentlyContinue).P)) {
-            $NugetPath = Join-Path $ENV:USERPROFILE nuget.exe
+try {
+    # Bootstrap nuget if we don't have it
+    if (-not ($NugetPath = (Get-Command 'nuget.exe' -ErrorAction SilentlyContinue).P)) {
+        $NugetPath = Join-Path $ENV:USERPROFILE nuget.exe
 
-            if(-not (Test-Path $NugetPath)) {
-                Invoke-WebRequest -uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile $NugetPath
-            }
+        if (-not (Test-Path $NugetPath)) {
+            Invoke-WebRequest -uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile $NugetPath
         }
+    }
 
-        # Bootstrap PSDepend, re-use nuget.exe for the module
-        if($path) {
-            $null = mkdir $path -Force
-        } 
+    # Bootstrap PSDepend, re-use nuget.exe for the module
+    if ($path) {
+        $null = mkdir $path -Force
+    } 
         
-        $NugetParams = 'install', 'PSDepend', '-Source', 'https://www.powershellgallery.com/api/v2/',
-                    '-ExcludeVersion', '-NonInteractive', '-OutputDirectory', $Path
-        & $NugetPath @NugetParams
+    $NugetParams = 'install', 'PSDepend', '-Source', 'https://www.powershellgallery.com/api/v2/',
+    '-ExcludeVersion', '-NonInteractive', '-OutputDirectory', $Path
+    & $NugetPath @NugetParams
 
-        Move-Item -Path $NugetPath -Destination "$(Join-Path $Path PSDepend)\nuget.exe" -Force
-    }
+    Move-Item -Path $NugetPath -Destination "$(Join-Path $Path PSDepend)\nuget.exe" -Force
+}
 
-    finally {
-        $ProgressPreference = $ExistingProgressPreference
-    }
-
+finally {
+    $ProgressPreference = $ExistingProgressPreference
+}

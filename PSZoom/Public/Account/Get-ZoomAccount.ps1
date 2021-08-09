ee63efc6336e56a5897c4ba4077fca06cd01d635
JosephMcEvoy <joe.maci@gmail.com>
2021-08-09 11:11:50 -0400

    Update Get-ZoomAccount.ps1

--- PSZoom/Public/Account/Get-ZoomAccount.ps1
+++ PSZoom/Public/Account/Get-ZoomAccount.ps1
@@ -51,9 +51,7 @@ function Get-ZoomAccount {
     process {
         $request = [System.UriBuilder]"https://api.zoom.us/v2/accounts/$id"
         $query = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
-        $request.Query = $query.ToString()
-        
-        Write-Output 'WARNING: you can only call this API if you have Master API permissions.'        
+        $request.Query = $query.ToString()      
         $response = Invoke-ZoomRestMethod -Uri $request.Uri -Headers ([ref]$Headers) -Method GET -ApiKey $ApiKey -ApiSecret $ApiSecret
         
         Write-Output $response        

e4fe60183df85ae7700412246a5896de755d27dc
Joseph McEvoy <joe.maci@gmail.com>
2022-08-07 21:23:20 -0400

    Add support for PowerShell 5.

--- PSZoom/Private/New-ZoomHeaders.ps1
+++ PSZoom/Private/New-ZoomHeaders.ps1
@@ -24,11 +24,21 @@ function New-ZoomHeaders {
     )
     
     Write-Verbose 'Generating Headers'
-    $tokenStr = ConvertFrom-SecureString -SecureString $Token -AsPlainText
+    if ($PSVersionTable.PSVersion.Major -ge 7) {
+        $tokenStr = ConvertFrom-SecureString -SecureString $Token -AsPlainText
+    } else {
+        $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Token)
+        $tokenStr = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+    }
+
     $Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
     $Headers.Add('content-type' , 'application/json')
     $Headers.Add('authorization', 'bearer ' + $tokenStr)
 
-    Write-Verbose $Headers
+    if ($PSVersionTable.PSVersion.Major -lt 7) {
+        # Clear plain string token.
+        [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR)
+    }
+
     Write-Output $Headers
 }

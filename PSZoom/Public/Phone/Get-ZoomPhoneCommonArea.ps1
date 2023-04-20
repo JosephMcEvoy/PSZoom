d5529945b33691081cbf8241a9cf07a2991f5077
Andrew Hoggins <andyhoggins@me.com>
2023-04-20 01:22:00 -0400

    Streamlined code using private cmdlet

--- PSZoom/Public/Phone/Get-ZoomPhoneCommonArea.ps1
+++ PSZoom/Public/Phone/Get-ZoomPhoneCommonArea.ps1
@@ -1,10 +1,13 @@
 <#
 
 .SYNOPSIS
-View specific site information in the Zoom Phone account.
+List Zoom Common Area phones that are associated with account Account.
 
 .DESCRIPTION
-View specific site information in the Zoom Phone account.
+List Zoom Common Area phones that are associated with account Account.
+
+.PARAMETER CommonAreaId
+ID number[s] of common area phones to be queried.
 
 .PARAMETER PageSize
 The number of records returned within a single API call (Min 30 - MAX 100).
@@ -13,19 +16,29 @@ The number of records returned within a single API call (Min 30 - MAX 100).
 The next page token is used to paginate through large result sets. A next page token will be returned whenever the set 
 of available results exceeds the current page size. The expiration period for this token is 15 minutes.
 
+.PARAMETER Full
+The full details of each Common Area Phone.
+
 .OUTPUTS
-An object with the Zoom API response.
+An array of Objects
 
 .EXAMPLE
-Retrieve a site's settings templates.
-Get-ZoomPhoneSite -SiteId ##########
+$AllData = Get-ZoomPhoneCommonArea
 
 .EXAMPLE
-Retrieve inforation for all sites.
-Get-ZoomPhoneSite
+$SomeData = Get-ZoomPhoneCommonArea -ObjectId $SpecificIDsToQuery
+
+.EXAMPLE
+$RawData = Get-ZoomPhoneCommonArea -PageSize 50 -NextPageToken $reponse.next_page_token
+
+.EXAMPLE
+$RawData = Get-ZoomPhoneCommonArea -PageSize 50
+
+.EXAMPLE
+$AllData = Get-ZoomPhoneCommonArea -Full
 
 .LINK
-https://developers.zoom.us/docs/api/rest/reference/phone/methods/#operation/listPhoneSites
+https://developers.zoom.us/docs/api/rest/reference/phone/methods/#operation/listCommonAreas
 
 #>
 
@@ -52,7 +65,6 @@ function Get-ZoomPhoneCommonArea {
         [Alias('next_page_token')]
         [string]$NextPageToken,
 
-        [parameter(ParameterSetName="SelectedRecord")]
         [parameter(ParameterSetName="AllData")]
         [switch]$Full = $False
 
@@ -66,117 +78,56 @@ function Get-ZoomPhoneCommonArea {
 
             "NextRecords" {
 
-                $request = [System.UriBuilder]$BASEURI
-                $query = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
-                $query.Add('page_size', $PageSize)
-                if ($NextPageToken) {
-                    $query.Add('next_page_token', $NextPageToken)
-                }
-                $request.Query = $query.ToString()
-                
-                $AggregatedResponse = Invoke-ZoomRestMethod -Uri $request.Uri -Method GET -ErrorAction Stop
-
+                $AggregatedResponse = Get-ZoomPaginatedData -URI $BASEURI -PageSize $PageSize -NextPageToken $NextPageToken
 
             }
             "SelectedRecord" {
 
-                $AggregatedResponse = @()
-
-                foreach ($id in $CommonAreaId) {
-                    $request = [System.UriBuilder]$BASEURI
-                    $request.path = "{0}/{1}" -f $request.path, $id
-                    $AggregatedResponse += Invoke-ZoomRestMethod -Uri $request.Uri -Method GET -ErrorAction Stop
-
-                }
+                $AggregatedResponse = Get-ZoomPaginatedData -URI $BASEURI -ObjectId $CommonAreaId
 
             }
             "AllData" {
 
-                $PageSize = 30
-                $AggregatedResponse = @()
-
-                do {
-
-                    $request = [System.UriBuilder]$BASEURI
-                    $query = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
-                    $query.Add('page_size', $PageSize)
-                    if ($response.next_page_token) {
-                        $query.Add('next_page_token', $response.next_page_token)
-                    }
-                    $request.Query = $query.ToString()
-                    
-                    $response = Invoke-ZoomRestMethod -Uri $request.Uri -Method GET -ErrorAction Stop
-
-                    if ($response.total_records -ne 0) {
-                        $AggregatedResponse += $response | Select-Object -ExpandProperty common_areas #| Select-Object @{Name="commonAreaId";Expression={$_.id}},site,display_name,extension_number,calling_plans,phone_numbers,desk_phones
-                    }
-
-                } until (!($response.next_page_token))
+                $AggregatedResponse = Get-ZoomPaginatedData -URI $BASEURI -PageSize $PageSize
 
             }
-
         }
+        
+        
+        
+        if ($Full -and ($PSVersionTable.PSVersion.Major -ge 7)) {
 
-        if ($Full -and ($PSVersionTable.PSVersion.Major -gt 10)) {
+            $TempAggregatedResponse = $AggregatedResponse
+            $AggregatedResponse = [System.Collections.Concurrent.ConcurrentBag[psobject]]::new()
+
+            $TempAggregatedResponse | ForEach-Object -Parallel {
+
+                $Script:PSZoomToken =  $using:PSZoomToken
+                $Script:ZoomURI = $using:ZoomURI
+                $localAggregatedResponse = $using:AggregatedResponse
+                import-module PSZoom
+
+                $localAggregatedResponse.Add($(Get-ZoomPhoneCommonArea -CommonAreaId $_.id))
+
+            } -ThrottleLimit 10  #Recommended amount for Pro users.  If 429 is returned this number may need to be lowered.  See Rate limit details.
+            
+
+        }elseif ($Full) {
 
             $TempAggregatedResponse = $AggregatedResponse
             $AggregatedResponse = @()
-            $TempAggregatedResponse | ForEach-Object -Parallel {
 
-                $AggregatedResponse += Get-ZoomPhoneCommonArea -CommonAreaId $_.id
+            $TempAggregatedResponse | ForEach-Object {
 
-            } -ThrottleLimit 15
+                
+                Write-Progress -Activity "Query Zoom API" -Status "$([math]::Round(($AggregatedResponse.count/$TempAggregatedResponse.count)*100))% Complete" -PercentComplete ([math]::Round(($AggregatedResponse.count/$TempAggregatedResponse.count)*100))
+                $AggregatedResponse += Get-ZoomPhoneCommonArea -CommonAreaId $_.Id
+                
 
-
-
-            <#
-            
-            $JobSb = {
-            param($obid)
-
-            # make api call
-
-            write-output = [PsCustomObject]@{
-                Obid = $obid
-                ApiResult = 0;
             }
-            }
-
-            # start jobs for all objects
-            Foreach ($object in $Collection) {
-                Start-Job -ScriptBlock $JobSB -ArgumentList $object.Id -Name $object.name
-            }
-
-            # wait for jobs to finish
-            while (get-job) {
-                Get-Job | Where-Object State -NE 'Running' | ForEach-Object {
-                    if ($_.State -ne 'Completed') {
-                        Write-Warning ('Job [{0}] [{1}] ended with state [{2}].' -f $_.id,$_.Name,$_.State)
-                    } else {
-                        $JobResults = @(Receive-Job $_)
-                    }
-
-                    # write results to datatable
-
-                    remove-job $_
-                }
-                start-sleep -Seconds 1
-            }
-
-
-            #>
-
-        } elseif ($Full -and ($PSVersionTable.PSVersion.Major -lt 7)) {
-
-            $TempAggregatedResponse = $AggregatedResponse
-            Clear-Variable AggregatedResponse
-            $AggregatedResponse = $TempAggregatedResponse | Get-ZoomPhoneCommonArea 
-
         }
         
         Write-Output $AggregatedResponse 
         
-
     }
-
 }
